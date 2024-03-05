09567db7fdc65b8896dd1a54a5e4b165c44b692d
khaerulfalah23 <khaerulfalah23@gmail.com>
2024-03-04 14:53:12 -1200

    refactor: create user

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,15 +1,22 @@
 const User = require('../models/UserModel');
 
 const createUser = async (req, res) => {
-  const user = req.body;
   // const query = { email: user.email };
   try {
+    const user = req.body;
+    const newUser = new User(user);
+    if (newUser) {
+      await newUser.save();
+      res.status(200).json(newUser);
+    } else {
+      res.status(400).json({ error: 'Invalid user data' });
+    }
     // const existingUser = await User.findOne(query);
     // if (existingUser) {
     //   return res.status(302).json({ message: 'User already exists!' });
     // }
-    const result = await User.create(user);
-    res.status(200).json(result);
+    // const result = await User.create(user);
+    // res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
