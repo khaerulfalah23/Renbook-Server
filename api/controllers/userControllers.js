ca688c2d7dfce5e5caf325e294aa962f7524ad24
khaerulfalah23 <khaerulfalah23@gmail.com>
2024-03-04 14:27:57 -1200

    refactor: comment find email

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -2,12 +2,12 @@ const User = require('../models/UserModel');
 
 const createUser = async (req, res) => {
   const user = req.body;
-  const query = { email: user.email };
+  // const query = { email: user.email };
   try {
-    const existingUser = await User.findOne(query);
-    if (existingUser) {
-      return res.status(302).json({ message: 'User already exists!' });
-    }
+    // const existingUser = await User.findOne(query);
+    // if (existingUser) {
+    //   return res.status(302).json({ message: 'User already exists!' });
+    // }
     const result = await User.create(user);
     res.status(200).json(result);
   } catch (error) {
